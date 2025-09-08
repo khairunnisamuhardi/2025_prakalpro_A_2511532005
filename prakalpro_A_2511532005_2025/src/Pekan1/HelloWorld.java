9210c12a84d9491642c7bae47cb903a5c836a869
muhardi <muhardi@localhost>
2025-09-08 15:01:41 +0700

    Praktikum 1 Kelas A

---
+++ prakalpro_A_2511532005_2025/src/Pekan1/HelloWorld.java
@@ -0,0 +1,18 @@
+package Pekan1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//program pertama
+		/*komentar
+		 * dua baris atau lebih
+		 * pertemuan 1 praktikum alpro
+		 */
+		 System.out.println ("Hello World");
+		 System.out.println ();
+		 System.out.println ("program ini menghasilkan");
+		 System.out.println ("empat baris output");
+		 System.out.println ("/ \\ // \\\\ /// \\\\");
+		 System.out.println ("This"+" program prints a\n"+"quotes from the Gettysburg Address.");
+	}
+}
