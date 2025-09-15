00033ec0f14f07196f4996de45822105228da06c
muhardi <muhardi@localhost>
2025-09-15 14:09:50 +0700

    Pratikum 2 Kelas A

---
+++ prakalpro_A_2511532005_2025/src/Pekan2/KelilingLingkaran.java
@@ -0,0 +1,10 @@
+package Pekan2;
+
+public class KelilingLingkaran {
+	public static void main (String[] args) {
+		final double PI = 3.14; /* Definisi konstanta */
+		double radius = 30; 	/* Deklarasi variabel */
+			System.out.println("keliling = " + 2 * PI * radius);
+	}
+
+}
