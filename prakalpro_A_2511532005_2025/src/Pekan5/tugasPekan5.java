1cfc67b690f49036fd70ad17ff6bd855bf9bfb25
muhardi <muhardi@localhost>
2025-11-02 04:01:28 +0700

    pekan5

--- prakalpro_A_2511532005_2025/src/Pekan5/tugasPekan5.java
+++ prakalpro_A_2511532005_2025/src/Pekan5/tugasPekan5.java
@@ -3,36 +3,29 @@ package Pekan5;
 public class tugasPekan5 {
     public static void main(String[] args) {
         int n = 4;
-        int width = 23; // jumlah "=" di border
-
-        // baris atas
+        int width = 23; 
+        
         System.out.print("#");
         for (int i = 0; i < width; i++) {
             System.out.print("=");
         }
         System.out.println("#");
 
-        // bagian atas
         for (int i = 1; i <= n; i++) {
             System.out.print("|");
 
-            // spasi kiri
             for (int j = 1; j < (n - i) * 2 + 2; j++) {
                 System.out.print(" ");
             }
 
-            // cetak <>
             System.out.print(" <> ");
 
-            // titik-titik
             for (int j = 1; j < i; j++) {
                 System.out.print("....");
             }
 
-            // cetak <>
             System.out.print(" <> ");
 
-            // spasi kanan
             for (int j = 1; j <= (n - i) * 2 + 2; j++) {
                 System.out.print(" ");
             }
@@ -40,27 +33,21 @@ public class tugasPekan5 {
             System.out.println("|");
         }
 
-        // bagian bawah
         for (int i = n; i >= 1; i--) {
             System.out.print("|");
 
-            // spasi kiri
             for (int j = 1; j <= (n - i) * 2 + 2; j++) {
                 System.out.print(" ");
             }
 
-            // cetak <>
             System.out.print("<> ");
 
-            // titik-titik
             for (int j = 1; j < i; j++) {
                 System.out.print("....");
             }
 
-            // cetak <>
             System.out.print(" <>");
 
-            // spasi kanan
             for (int j = 1; j <= (n - i) * 2 + 2; j++) {
                 System.out.print(" ");
             }
@@ -68,7 +55,6 @@ public class tugasPekan5 {
             System.out.println(" |");
         }
 
-        // baris bawah
         System.out.print("#");
         for (int i = 0; i < width; i++) {
             System.out.print("=");
