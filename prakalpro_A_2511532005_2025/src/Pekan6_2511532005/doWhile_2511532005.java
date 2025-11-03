d3425d2fb2595bf312e459ef0265d1a678c8c8c0
muhardi <muhardi@localhost>
2025-11-03 14:46:54 +0700

    PraktikumPekan6_2511532005

--- prakalpro_A_2511532005_2025/src/Pekan6_2511532005/doWhile_2511532005.java
+++ prakalpro_A_2511532005_2025/src/Pekan6_2511532005/doWhile_2511532005.java
@@ -1,5 +1,19 @@
 package Pekan6_2511532005;
 
+import java.util.Scanner;
+
 public class doWhile_2511532005 {
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		
+		String phrase;
+		do {
+			System.out.print("Input Password: ");
+			phrase = console.next();
+		} while (!phrase.equals("abcd"));
+		
+	}
+	
+	
 
 }
